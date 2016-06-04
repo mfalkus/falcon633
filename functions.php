03ceaeb8627346ba2ab38f7fb8097fcf119c9aaf
Martin Falkus <martin@falkus.co>
2016-06-04 10:19:01 +0100

    Fix pagination styles as per design

--- functions.php
+++ functions.php
@@ -123,7 +123,9 @@ function falcon_pagination() {
             'base'      => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
             'format'    => '?paged=%#%',
             'current'   => max( 1, get_query_var('paged') ),
-            'total'     => $wp_query->max_num_pages
+            'total'     => $wp_query->max_num_pages,
+            'prev_text' => __('« '),
+            'next_text' => __(' »'),
         ) )
         . '</div>';
 }
