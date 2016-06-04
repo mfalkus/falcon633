08bf30790c9502cc5d9683c006ce9f0d67881d58
Martin Falkus <martin@falkus.co>
2016-06-04 15:09:09 +0100

    Add favicon, ICO format

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, minimum-scale=1.0">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-    <link rel="shortcut icon" href="<?php bloginfo('template_directory') ?>/favicon.png"  type="image/png" />
+    <link rel="shortcut icon" href="<?php bloginfo('template_directory') ?>/favicon.ico"  />
 
 <?php wp_head(); ?>
 </head>
