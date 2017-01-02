94f62f30f01ee2b367fbf141293f4d41bdf7bbd6
Martin Falkus <martin@falkus.co>
2017-01-02 21:21:29 +0000

    Add basic 404 page

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * A basic 404 page...
+ * @package Falcon633
+ */
+
+get_header(); ?>
+
+<div class="island text--center">
+<h3><code>That doesn't appear to be a valid URL...</code></h3>
+</div>
+
+<?php get_footer(); ?>
